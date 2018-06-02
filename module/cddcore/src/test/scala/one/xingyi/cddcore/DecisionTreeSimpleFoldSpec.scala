84b2b944b2e1aa50eec1769b8f53083edb99e7c1
Phil <philip.rice2@ing.com>
2018-06-02 21:56:58 +0200

    tidy

--- module/cddcore/src/test/scala/one/xingyi/cddcore/DecisionTreeSimpleFoldSpec.scala
+++ module/cddcore/src/test/scala/one/xingyi/cddcore/DecisionTreeSimpleFoldSpec.scala
@@ -17,7 +17,7 @@ class DecisionTreeSimpleFoldSpec extends CddSpec with DecisionTreeFixture {
   behavior of "Decision tree folding"
 
   it should "fold an empty list of scenarios and just have the default" in {
-    folder(List()) shouldBe concEmpty
+    folder(List()) shouldBe cEmpty
   }
   it should "fold a scenario and have a conclusion with just that scenario" in {
     folder(List(snormal)) shouldBe concNormal
@@ -31,9 +31,9 @@ class DecisionTreeSimpleFoldSpec extends CddSpec with DecisionTreeFixture {
   }
 
   it should "put the scenario on the right when a different scenario without when is added" in {
-    folder(List(sa, sbwb)) shouldBe dn(sbwb, sa)
-    folder(List(sa, sa2, sax, sbwb)) shouldBe dt(List(sbwb), List(sa, sa2, sax))
-    folder(List(sa, sa2, sax, sbwb, sb)) shouldBe dt(List(sbwb, sb), List(sa, sa2, sax))
+    folder(List(sa, sbwb)) shouldBe d(sbwb, sa)
+    folder(List(sa, sa2, sax, sbwb)) shouldBe d(sbwb.logic, c(sa, sa2, sax), c(sbwb))
+    folder(List(sa, sa2, sax, sbwb, sb)) shouldBe d(sbwb.logic, c(sa, sa2, sax), c(sbwb, sb))
   }
 
   it should "change the logic to use the new scenario if existing scenario hasn't a reason" in {
@@ -42,9 +42,9 @@ class DecisionTreeSimpleFoldSpec extends CddSpec with DecisionTreeFixture {
 
   }
   it should "put a scenario without when on the left when a different scenario with when is added" in {
-    folder(List(sbwb, sa)) shouldBe dn(sbwb, sa)
-    folder(List(sbw, sbwb, sa)) shouldBe DecisionNode(sbwb.logic, cn(sa), ConclusionNode(List(sbw, sbwb), sbwb.logic))
-    folder(List(sb, sbwb, sa)) shouldBe DecisionNode(sbwb.logic, cn(sa), ConclusionNode(List(sb, sbwb), sbwb.logic))
+    folder(List(sbwb, sa)) shouldBe d(sbwb, sa)
+    folder(List(sbw, sbwb, sa)) shouldBe DecisionNode(sbwb.logic, c(sa), ConclusionNode(List(sbw, sbwb), sbwb.logic))
+    folder(List(sb, sbwb, sa)) shouldBe DecisionNode(sbwb.logic, c(sa), ConclusionNode(List(sb, sbwb), sbwb.logic))
   }
 
   it should "make a composite reason node if new reason given" in {
@@ -55,10 +55,10 @@ class DecisionTreeSimpleFoldSpec extends CddSpec with DecisionTreeFixture {
   }
 
   it should "make a decision node with scenario as condition if conclusion and situation come to different results, and conclusion has no condition" in {
-    folder(List(sa, sbwb)) shouldBe dn(sbwb, sa)
+    folder(List(sa, sbwb)) shouldBe d(sbwb, sa)
   }
   it should "make a decision node with scenario as condition if conclusion and situation come to different results, and conclusion has no condition, and keep supporting scenarios " in {
-    folder(List(sa, sax, sa2, sbwb)) shouldBe DecisionNode(sbwb.logic, ConclusionNode(List(sa, sax, sa2), sa.logic), cn(sbwb))
+    folder(List(sa, sax, sa2, sbwb)) shouldBe d(sbwb.logic, c(sa, sax, sa2), c(sbwb))
   }
 
   it should "not accept a scenario if it cannot be added because it comes to a different conclusion without a reason in conclusion or scenario" in {
--- module/cddcore/src/test/scala/one/xingyi/cddcore/ScenarioFixture.scala
+++ module/cddcore/src/test/scala/one/xingyi/cddcore/ScenarioFixture.scala
@@ -2,7 +2,7 @@ package one.xingyi.cddcore
 
 import one.xingyi.cddcore.NullScenarioAggregator._
 import one.xingyi.cddcore.UntypedScenarioBuilder._
-import one.xingyi.cddutilities.CddSpec
+import one.xingyi.cddutilities.{CddSpec, DefinedInSourceCodeAt}
 import org.scalatest.Matchers
 
 import scala.language.postfixOps
@@ -34,28 +34,37 @@ trait ScenarioFixture extends Matchers {
   val sabwab = scenario("ab") produces "AB" when (_ contains "b") scenario
 }
 
+trait DecisionTreeLanguage[P, R] {
+  val cEmpty = ConclusionNode[P, R](List(), ScenarioLogic.empty)
+  def s(p: P, r: R) = Scenario[P, R](p, ScenarioLogic.empty, List(), EngineComponentData(DefinedInSourceCodeAt.definedInSourceCodeAt(), Some(s"$p => $r")))
+  def s(p: P, r: R, b: P => Boolean) = Scenario[P, R](p, SingleScenarioLogic[P, R](Some(r), Some(b), None, DefinedInSourceCodeAt.definedInSourceCodeAt()), List(), EngineComponentData(DefinedInSourceCodeAt.definedInSourceCodeAt(), Some(s"$p => $r")))
 
-trait DecisionTreeFixture extends ScenarioFixture {
-  def cn[P, R](s: Scenario[P, R]) = ConclusionNode(List(s), s.logic)
-  def cn[P, R](s: List[Scenario[P, R]]) = ConclusionNode(s, s.head.logic)
-  def dn[P, R](scond: Scenario[P, R], selse: Scenario[P, R]) = DecisionNode(scond.logic, cn(selse), cn(scond))
-  def dt[P, R](scond: List[Scenario[P, R]], selse: List[Scenario[P, R]]) = DecisionNode(scond.head.logic, cn(selse), cn(scond))
+  def c(s: Scenario[P, R], ss: Scenario[P, R]*) = ConclusionNode(s :: ss.toList, s.logic)
+  def c(logic: ScenarioLogic[P, R], ss: Scenario[P, R]*) = ConclusionNode(ss.toList, logic)
+  def d(right: Scenario[P, R], left: Scenario[P, R]) = DecisionNode(right.logic, c(left), c(right))
+  def d(logic: ScenarioLogic[P, R], left: DecisionTreeNode[P, R], right: DecisionTreeNode[P, R]) = DecisionNode(logic, left, right)
+  def d(logic: Scenario[P, R], left: DecisionTreeNode[P, R], right: DecisionTreeNode[P, R]) = DecisionNode(logic.logic, left, right)
+  def t(dn: DecisionTreeNode[P, R]) = DecisionTree(dn, List())
 
-  val concEmpty = ConclusionNode[String, String](List(), ScenarioLogic.empty)
-  val concNormal = ConclusionNode(List(snormal), snormal.logic)
-  val concNormal1And2 = ConclusionNode(List(snormal, snormal2), snormal.logic)
-  val concNoPassport = ConclusionNode(List(sNoPassport), sNoPassport.logic)
-  val concGunNoPassport = ConclusionNode(List(sgunNoPassport), sgunNoPassport.logic)
-  val concGun = ConclusionNode(List(sgun), sgun.logic)
-  val concGunGunNoPassport = ConclusionNode(List(sgun, sgunNoPassport), sgun.logic)
-  val dNormalPassport = DecisionNode(sNoPassport.logic, concNormal, concNoPassport)
-  val dNormalGun = DecisionNode(sgun.logic, concNormal, concGun)
-  val dGunNoPassword = DecisionNode(sNoPassport.logic, concGun, concNoPassport)
+}
 
-  val conca = ConclusionNode(List(sa), sa.logic)
-  val concawa = ConclusionNode(List(sawa), sawa.logic)
-  val concb = ConclusionNode(List(sb), sb.logic)
-  val concbwb = ConclusionNode(List(sbwb), sbwb.logic)
+trait DecisionTreeFixture extends ScenarioFixture with DecisionTreeLanguage[String, String] {
+
+
+  val concNormal = c(snormal)
+  val concNormal1And2 = c(snormal, snormal2)
+  val concNoPassport = c(sNoPassport)
+  val concGunNoPassport = c(sgunNoPassport)
+  val concGun = c(sgun)
+  val concGunGunNoPassport = c(sgun, sgunNoPassport)
+  val dNormalPassport = d(sNoPassport.logic, concNormal, concNoPassport)
+  val dNormalGun = d(sgun.logic, concNormal, concGun)
+  val dGunNoPassword = d(sNoPassport.logic, concGun, concNoPassport)
+
+  val conca = c(sa)
+  val concawa = c(sawa)
+  val concb = c(sb)
+  val concbwb = c(sbwb)
 }
 
 class ScenarioFixtureSetupSpec extends CddSpec with ScenarioFixture {
